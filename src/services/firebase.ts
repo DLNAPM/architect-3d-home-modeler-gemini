0abe59ced94da2162f261585015c10874b8df943
DLNAPM <DLaniger.Napm.Consulting@gmail.com>
2025-12-07 01:02:15 -0500

    refactor: Initialize Firebase app
    The Firebase initialization was previously commented out, but it is now being re-enabled to potentially support future features.

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,4 +1,5 @@
-// Firebase initialization has been disabled as user authentication is no longer used.
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD9wVfpTCCLojE-yRIFzNoJOID1jp9IwzY",
   authDomain: "architect-3d-home-modeler.firebaseapp.com",
@@ -8,5 +9,7 @@ const firebaseConfig = {
   appId: "1:762702816387:web:a25dc9f358b8bf45ce67b6",
   measurementId: "G-ZMZ7K9SQ72"
 };
+
+const app = initializeApp(firebaseConfig);
 
 export { app };
